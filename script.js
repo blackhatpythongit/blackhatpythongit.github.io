3fe29119365a197d1099700e3ea79915675df9d3
blackhatpythongit <testgit@protonmail.com>
2016-09-16 13:59:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,37 @@
 window.onload = init;
-
-function init(){
-	alert(1);
-	document.getElementsByTagName("form")[0].onsubmit = calc
+function init() {
+  alert(1);
+  document.getElementsByTagName('form') [0].onsubmit = calc
 }
-
-function calc(){
-	var string = document.getElementById("string").value;
-	document.getElementById("string").value = op(string);
-	return false;
+function calc() {
+  var string = document.getElementById('string').value;
+  document.getElementById('string').value = op(string);
+  return false;
 }
-
-function op(string){
-	var actionType = "";
-	var radioButtonSet = document.getElementsByTagName("form")[0].actionType;
-	for (var i=0; i<radioButtonSet.length; i++)
-	{
-		if (radioButtonSet[i].checked) {
-			actionType = radioButtonSet[i].actionType;
-		}
-	}
-
-	switch(actionType){
-		case "chr2binary":
-		return chr2binary(string);
-		case "binary2chr":
-		return binary2chr(string);
-		default:
-		alert("No valid action was chosen");
-		return "";
-	}
+function op(string) {
+  var actionType = '';
+  var radioButtonSet = document.getElementsByTagName('form') [0].actionType;
+  for (var i = 0; i < radioButtonSet.length; i++)
+  {
+    if (radioButtonSet[i].checked) {
+      actionType = radioButtonSet[i].actionType;
+    }
+  }
+  switch (actionType) {
+    case 'chr2binary':
+      return chr2binary(string);
+    case 'binary2chr':
+      return binary2chr(string);
+    default:
+      alert('No valid action was chosen');
+      return '';
+  }
 }
-
 function chr2binary(string)
 {
-	return (String.charCodeAt(string)).toString(2)
+  return (String.charCodeAt(string)).toString(2);
 }
-
 function binary2chr(string)
 {
-	return (String.fromcharCode(parseInt(string, 2)))
+  return (String.fromcharCode(parseInt(string, 2)));
 }
